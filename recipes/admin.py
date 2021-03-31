2b931ea38a92334ea15eefd0194c666bd7d17dc8
Y4SSIN <yassin_developer@outlook.com>
2021-03-31 13:45:51 +0200

    Customize admin panel for recipes

--- recipes/admin.py
+++ recipes/admin.py
@@ -3,6 +3,22 @@ from .models import Recipe, Category, Difficulty
 
 # Register your models here.
 
-admin.site.register(Recipe)
+
+class RecipeAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'category__name', 'difficulty__name',
+                     'total_servings', 'rating', 'preparation_time',)
+    list_filter = ('category', 'difficulty',)
+    ordering = ('-date_posted',)
+    list_display = ('title', 'total_servings', 'preparation_time',
+                    'category', 'difficulty', 'rating')
+    fieldsets = (
+        ('About', {'fields': ('title', 'description',)}),
+        ('Requirements', {'fields': ('total_servings', 'preparation_time',)}),
+        ('Description', {'fields': ('rating', 'category', 'difficulty',)}),
+        ('Chef', {'fields': ('chef',)}),
+    )
+
+
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Category)
 admin.site.register(Difficulty)
