c0edac717b2db193104501d927ee55c5266b9990
yassinao <yassin.freelancer@gmail.com>
2021-05-30 19:38:41 +0200

    Event query setup

--- events/schemas.py
+++ events/schemas.py
@@ -20,3 +20,23 @@ class EventHostType(DjangoObjectType):
     class Meta:
         model = User
         fields = ('username',)
+
+
+class Query(graphene.ObjectType):
+
+    all_events = graphene.List(EventType)
+    event_by_id = graphene.Field(EventType, id=graphene.Int())
+    event_by_category = graphene.List(EventType, category=graphene.Int())
+    event_by_host = graphene.List(EventType, host=graphene.Int())
+
+    def resolve_all_events(self, info, **kwargs):
+        return Event.objects.all()
+
+    def resolve_event_by_id(self, info, id):
+        return Event.objects.get(pk=id)
+
+    def resolve_event_by_category(self, info, category):
+        return Event.object.filter(category=category)
+
+    def resolve_event_by_host(self, info, host):
+        return Event.objects.filter(host=host)
